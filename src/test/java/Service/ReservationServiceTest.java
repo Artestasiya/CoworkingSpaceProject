c6d21f3066296a854b95593f999a7c0ab98a065e
Artestasiya <anastasialoilopest@gmail.com>
2025-03-19 20:21:39 +0300

    Mockito

--- src/test/java/Service/ReservationServiceTest.java
+++ src/test/java/Service/ReservationServiceTest.java
@@ -2,40 +2,57 @@ package Service;
 
 import Data.CoworkingSpace;
 import Data.CoworkingType;
-import Data.Reservation;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.io.File;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class ReservationServiceTest {
 
-    private ReservationService reservationService;
+    @Mock
     private CoworkingSpaceService spaceService;
+
+    @InjectMocks
+    private ReservationService reservationService;
 
     @BeforeEach
     void setUp() {
-        spaceService = new CoworkingSpaceService();
+        MockitoAnnotations.openMocks(this);
+        File file = new File("reservations.dat");
+        if (file.exists()) {
+            file.delete();
+        }
         reservationService = new ReservationService(spaceService);
     }
 
     @Test
     void testAddReservationToNonExistentSpace() {
+        when(spaceService.getSpaceById(999)).thenReturn(Optional.empty());
         reservationService.addReservation("user1", "2023-10-01", "10:00", "12:00", 999);
         assertEquals(0, reservationService.getReservationsByUser("user1").size());
     }
 
     @Test
     void testAddReservationToUnavailableSpace() {
-        spaceService.addSpace(CoworkingType.OPEN_SPACE, 100.0, false);
+        CoworkingSpace space = new CoworkingSpace(1, CoworkingType.OPEN_SPACE, 100.0, false);
+        when(spaceService.getSpaceById(1)).thenReturn(Optional.of(space));
         reservationService.addReservation("user1", "2023-10-01", "10:00", "12:00", 1);
         assertEquals(0, reservationService.getReservationsByUser("user1").size());
     }
 
     @Test
     void testCancelReservation() {
-        spaceService.addSpace(CoworkingType.OPEN_SPACE, 100.0, true);
+        CoworkingSpace space = new CoworkingSpace(1, CoworkingType.OPEN_SPACE, 100.0, true);
+        when(spaceService.getSpaceById(1)).thenReturn(Optional.of(space));
         reservationService.addReservation("user1", "2023-10-01", "10:00", "12:00", 1);
+        assertEquals(1, reservationService.getReservationsByUser("user1").size());
         reservationService.cancelReservation(1);
         assertEquals(0, reservationService.getReservationsByUser("user1").size());
     }
